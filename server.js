f23bb20b1e53cecec282e8b9694e80f7c612030c
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-03-12 15:56:02 +0000

    made a routes folder and completed html routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express =require('express')
+const htmlRoutes= require("./routes/htmlRoutes")
 
 
 const PORT= process.env.PORT || 8080
@@ -8,6 +9,8 @@ const app= express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
+
+app.use(htmlRoutes)
 
 
 
