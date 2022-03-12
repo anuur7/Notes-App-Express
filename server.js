58fbac2ec99163353e28fbd2df05beb0670b7f4e
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-03-12 16:05:38 +0000

    made a .getapi router and required the files needed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const htmlRoutes= require("./routes/htmlRoutes")
+const apiRoutes= require("./routes/apiRoutes")
 
 
 const PORT= process.env.PORT || 8080
@@ -9,7 +10,7 @@ const app= express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
-
+app.use(apiRoutes)
 app.use(htmlRoutes)
 
 
